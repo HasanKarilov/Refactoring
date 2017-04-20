3bd74a9f48aa621af9822ce4096579668ec52ba4
HasanKarilov <hkarilov@gmail.com>
2017-04-20 14:11:00 +0600

    Замена параметра набором специализированных методов 06

--- human/Student.java
+++ human/Student.java
@@ -19,7 +19,6 @@ public class Student extends UniversityPerson {
 
     public void learn() {
     }
-
     public void incAverageGradeBy01() {
         averageGrade += 0.1;
     }
@@ -28,17 +27,15 @@ public class Student extends UniversityPerson {
         averageGrade += 0.2;
     }
 
-    public void setValue(String name, double value) {
-        if (name.equals("averageGrade")) {
-            averageGrade = value;
-            return;
-        }
-        if (name.equals("course")) {
-            course = (int) value;
-            return;
-        }
+    public void setAverageGrade(double averageGrade)
+    {
+        this.averageGrade = averageGrade;
     }
 
+    public void setCourse(int course)
+    {
+        this.course = course;
+    }
     public void setBeginningOfSession(int day, int month, int year) {
         beginningOfSession = new Date(year, month, day);
     }
