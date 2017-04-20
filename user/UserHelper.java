253136c34b99086864213ed0f2906532703a7288
HasanKarilov <hkarilov@gmail.com>
2017-04-20 16:37:25 +0600

    This commit is not important 15last

--- user/UserHelper.java
+++ user/UserHelper.java
@@ -2,11 +2,13 @@ package com.javarush.task.task29.task2909.user;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class UserHelper {
+public class UserHelper
+{
     private User userAnya = new User("Аня", "Смирнова", 10);
     private User userRoma = new User("Рома", "Виноградов", 30);
 
-    public void printUsers() {
+    public void printUsers()
+    {
         userAnya.printInfo();
         userAnya.printAdditionalInfo();
 
@@ -14,22 +16,20 @@ public class UserHelper {
         userRoma.printAdditionalInfo();
     }
 
-    public int calculateAverageAge() {
+    public int calculateAvarageAge()
+    {
         User userUra = new User("Юра", "Карп", 28);
 
         return (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
     }
 
-    public int calculateRate(AtomicInteger base, int age, boolean hasWork, boolean hasHouse) {
-        int result = base.get();
-        result += age / 100;
-        result *= hasWork ? 1.1 : 0.9;
-        result *= hasHouse ? 1.1 : 0.9;
-        return result;
+    public double calculateRate(double base, int age, boolean hasWork, boolean hasHouse)
+    {
+        return (base + (age / 100)) * (hasWork ? 1.1 : 0.9) * (hasHouse ? 1.1 : 0.9);
     }
 
-
-    public String getBossName(User user) {
+    public String getBoosName(User user)
+    {
         return user.getBoss();
     }
 }
