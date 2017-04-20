325d6e2f810c4cc9e08cf1662b1ea8ba9319ddc1
HasanKarilov <hkarilov@gmail.com>
2017-04-20 15:01:14 +0600

    Самоинкапсуляция поля, Замена поля-массива объектом, Замена кодирования типа классом 09

--- human/Human.java
+++ human/Human.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class Human implements Alive{
+    public class Size
+    {
+        public int height;
+        public int weight;
+    }
 
     private List<Human> children = new ArrayList<>();
 
@@ -13,14 +18,9 @@ public class Human implements Alive{
     protected int age;
     protected String name;
 
-    protected int[] size;
+    protected Size size;
 
-
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
     public List<Human> getChildren()
     {
@@ -38,12 +38,13 @@ public class Human implements Alive{
             children.remove(human);
         }
     }
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
     }
 
-    public int getBloodGroup() {
-        return bloodGroup;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
     public Human(String name, int age)
@@ -74,8 +75,9 @@ public class Human implements Alive{
         return id;
     }
 
-    public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+    public void printSize()
+    {
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     @Override
